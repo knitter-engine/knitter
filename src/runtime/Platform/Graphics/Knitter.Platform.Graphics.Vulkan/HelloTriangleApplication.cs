5b5917c8089afb41b185ddc68506b1226a6dccaf
ke152 <ke152@foxmail.com>
2023-11-18 08:11:31 +0800

    Follow SilkVulkanTutorial Lesson 10

--- src/runtime/Platform/Graphics/Knitter.Platform.Graphics.Vulkan/HelloTriangleApplication.cs
+++ src/runtime/Platform/Graphics/Knitter.Platform.Graphics.Vulkan/HelloTriangleApplication.cs
@@ -21,16 +21,28 @@ struct QueueFamilyIndices
     }
 }
 
+struct SwapChainSupportDetails
+{
+    public SurfaceCapabilitiesKHR Capabilities;
+    public SurfaceFormatKHR[] Formats;
+    public PresentModeKHR[] PresentModes;
+}
+
 public unsafe class HelloTriangleApplication
 {
     const int WIDTH = 800;
     const int HEIGHT = 600;
 
-    bool EnableValidationLayers = false;
+    bool EnableValidationLayers = true;
 
     private readonly string[] validationLayers = new[]
     {
         "VK_LAYER_KHRONOS_validation"
+    };
+
+    private readonly string[] deviceExtensions = new[]
+    {
+        KhrSwapchain.ExtensionName
     };
 
     private IWindow? window;
@@ -48,6 +60,15 @@ public unsafe class HelloTriangleApplication
 
     private Queue graphicsQueue;
     private Queue presentQueue;
+
+    private KhrSwapchain? khrSwapChain;
+    private SwapchainKHR swapChain;
+    private Image[]? swapChainImages;
+    private Format swapChainImageFormat;
+    private Extent2D swapChainExtent;
+    private ImageView[]? swapChainImageViews;
+
+    private PipelineLayout pipelineLayout;
 
     public void Run()
     {
@@ -82,6 +103,9 @@ public unsafe class HelloTriangleApplication
         CreateSurface();
         PickPhysicalDevice();
         CreateLogicalDevice();
+        CreateSwapChain();
+        CreateImageViews();
+        CreateGraphicsPipeline();
     }
 
     private void MainLoop()
@@ -91,6 +115,15 @@ public unsafe class HelloTriangleApplication
 
     private void CleanUp()
     {
+        vk!.DestroyPipelineLayout(device, pipelineLayout, null);
+
+        foreach (var imageView in swapChainImageViews!)
+        {
+            vk!.DestroyImageView(device, imageView, null);
+        }
+
+        khrSwapChain!.DestroySwapchain(device, swapChain, null);
+
         vk!.DestroyDevice(device, null);
 
         if (EnableValidationLayers)
@@ -266,7 +299,8 @@ public unsafe class HelloTriangleApplication
 
             PEnabledFeatures = &deviceFeatures,
 
-            EnabledExtensionCount = 0
+            EnabledExtensionCount = (uint)deviceExtensions.Length,
+            PpEnabledExtensionNames = (byte**)SilkMarshal.StringArrayToPtr(deviceExtensions)
         };
 
         if (EnableValidationLayers)
@@ -291,13 +325,397 @@ public unsafe class HelloTriangleApplication
         {
             SilkMarshal.Free((nint)createInfo.PpEnabledLayerNames);
         }
+
+        SilkMarshal.Free((nint)createInfo.PpEnabledExtensionNames);
+
+    }
+
+    private void CreateSwapChain()
+    {
+        var swapChainSupport = QuerySwapChainSupport(physicalDevice);
+
+        var surfaceFormat = ChooseSwapSurfaceFormat(swapChainSupport.Formats);
+        var presentMode = ChoosePresentMode(swapChainSupport.PresentModes);
+        var extent = ChooseSwapExtent(swapChainSupport.Capabilities);
+
+        var imageCount = swapChainSupport.Capabilities.MinImageCount + 1;
+        if (swapChainSupport.Capabilities.MaxImageCount > 0 && imageCount > swapChainSupport.Capabilities.MaxImageCount)
+        {
+            imageCount = swapChainSupport.Capabilities.MaxImageCount;
+        }
+
+        SwapchainCreateInfoKHR creatInfo = new()
+        {
+            SType = StructureType.SwapchainCreateInfoKhr,
+            Surface = surface,
+
+            MinImageCount = imageCount,
+            ImageFormat = surfaceFormat.Format,
+            ImageColorSpace = surfaceFormat.ColorSpace,
+            ImageExtent = extent,
+            ImageArrayLayers = 1,
+            ImageUsage = ImageUsageFlags.ColorAttachmentBit,
+        };
+
+        var indices = FindQueueFamilies(physicalDevice);
+        var queueFamilyIndices = stackalloc[] { indices.GraphicsFamily!.Value, indices.PresentFamily!.Value };
+
+        if (indices.GraphicsFamily != indices.PresentFamily)
+        {
+            creatInfo = creatInfo with
+            {
+                ImageSharingMode = SharingMode.Concurrent,
+                QueueFamilyIndexCount = 2,
+                PQueueFamilyIndices = queueFamilyIndices,
+            };
+        }
+        else
+        {
+            creatInfo.ImageSharingMode = SharingMode.Exclusive;
+        }
+
+        creatInfo = creatInfo with
+        {
+            PreTransform = swapChainSupport.Capabilities.CurrentTransform,
+            CompositeAlpha = CompositeAlphaFlagsKHR.OpaqueBitKhr,
+            PresentMode = presentMode,
+            Clipped = true,
+
+            OldSwapchain = default
+        };
+
+        if (!vk!.TryGetDeviceExtension(instance, device, out khrSwapChain))
+        {
+            throw new NotSupportedException("VK_KHR_swapchain extension not found.");
+        }
+
+        if (khrSwapChain!.CreateSwapchain(device, creatInfo, null, out swapChain) != Result.Success)
+        {
+            throw new Exception("failed to create swap chain!");
+        }
+
+        khrSwapChain.GetSwapchainImages(device, swapChain, ref imageCount, null);
+        swapChainImages = new Image[imageCount];
+        fixed (Image* swapChainImagesPtr = swapChainImages)
+        {
+            khrSwapChain.GetSwapchainImages(device, swapChain, ref imageCount, swapChainImagesPtr);
+        }
+
+        swapChainImageFormat = surfaceFormat.Format;
+        swapChainExtent = extent;
+    }
+
+    private void CreateImageViews()
+    {
+        swapChainImageViews = new ImageView[swapChainImages!.Length];
+
+        for (int i = 0; i < swapChainImages.Length; i++)
+        {
+            ImageViewCreateInfo createInfo = new()
+            {
+                SType = StructureType.ImageViewCreateInfo,
+                Image = swapChainImages[i],
+                ViewType = ImageViewType.Type2D,
+                Format = swapChainImageFormat,
+                Components =
+                {
+                    R = ComponentSwizzle.Identity,
+                    G = ComponentSwizzle.Identity,
+                    B = ComponentSwizzle.Identity,
+                    A = ComponentSwizzle.Identity,
+                },
+                SubresourceRange =
+                {
+                    AspectMask = ImageAspectFlags.ColorBit,
+                    BaseMipLevel = 0,
+                    LevelCount = 1,
+                    BaseArrayLayer = 0,
+                    LayerCount = 1,
+                }
+
+            };
+
+            if (vk!.CreateImageView(device, createInfo, null, out swapChainImageViews[i]) != Result.Success)
+            {
+                throw new Exception("failed to create image views!");
+            }
+        }
+    }
+
+    private void CreateGraphicsPipeline()
+    {
+        var vertShaderCode = File.ReadAllBytes("shaders/vert.spv");
+        var fragShaderCode = File.ReadAllBytes("shaders/frag.spv");
+
+        var vertShaderModule = CreateShaderModule(vertShaderCode);
+        var fragShaderModule = CreateShaderModule(fragShaderCode);
+
+        PipelineShaderStageCreateInfo vertShaderStageInfo = new()
+        {
+            SType = StructureType.PipelineShaderStageCreateInfo,
+            Stage = ShaderStageFlags.VertexBit,
+            Module = vertShaderModule,
+            PName = (byte*)SilkMarshal.StringToPtr("main")
+        };
+
+        PipelineShaderStageCreateInfo fragShaderStageInfo = new()
+        {
+            SType = StructureType.PipelineShaderStageCreateInfo,
+            Stage = ShaderStageFlags.FragmentBit,
+            Module = fragShaderModule,
+            PName = (byte*)SilkMarshal.StringToPtr("main")
+        };
+
+        var shaderStages = stackalloc[]
+        {
+            vertShaderStageInfo,
+            fragShaderStageInfo
+        };
+
+        PipelineVertexInputStateCreateInfo vertexInputInfo = new()
+        {
+            SType = StructureType.PipelineVertexInputStateCreateInfo,
+            VertexBindingDescriptionCount = 0,
+            VertexAttributeDescriptionCount = 0,
+        };
+
+        PipelineInputAssemblyStateCreateInfo inputAssembly = new()
+        {
+            SType = StructureType.PipelineInputAssemblyStateCreateInfo,
+            Topology = PrimitiveTopology.TriangleList,
+            PrimitiveRestartEnable = false,
+        };
+
+        Viewport viewport = new()
+        {
+            X = 0,
+            Y = 0,
+            Width = swapChainExtent.Width,
+            Height = swapChainExtent.Height,
+            MinDepth = 0,
+            MaxDepth = 1,
+        };
+
+        Rect2D scissor = new()
+        {
+            Offset = { X = 0, Y = 0 },
+            Extent = swapChainExtent,
+        };
+
+        PipelineViewportStateCreateInfo viewportState = new()
+        {
+            SType = StructureType.PipelineViewportStateCreateInfo,
+            ViewportCount = 1,
+            PViewports = &viewport,
+            ScissorCount = 1,
+            PScissors = &scissor,
+        };
+
+        PipelineRasterizationStateCreateInfo rasterizer = new()
+        {
+            SType = StructureType.PipelineRasterizationStateCreateInfo,
+            DepthClampEnable = false,
+            RasterizerDiscardEnable = false,
+            PolygonMode = PolygonMode.Fill,
+            LineWidth = 1,
+            CullMode = CullModeFlags.BackBit,
+            FrontFace = FrontFace.Clockwise,
+            DepthBiasEnable = false,
+        };
+
+        PipelineMultisampleStateCreateInfo multisampling = new()
+        {
+            SType = StructureType.PipelineMultisampleStateCreateInfo,
+            SampleShadingEnable = false,
+            RasterizationSamples = SampleCountFlags.Count1Bit,
+        };
+
+        PipelineColorBlendAttachmentState colorBlendAttachment = new()
+        {
+            ColorWriteMask = ColorComponentFlags.RBit | ColorComponentFlags.GBit | ColorComponentFlags.BBit | ColorComponentFlags.ABit,
+            BlendEnable = false,
+        };
+
+        PipelineColorBlendStateCreateInfo colorBlending = new()
+        {
+            SType = StructureType.PipelineColorBlendStateCreateInfo,
+            LogicOpEnable = false,
+            LogicOp = LogicOp.Copy,
+            AttachmentCount = 1,
+            PAttachments = &colorBlendAttachment,
+        };
+
+        colorBlending.BlendConstants[0] = 0;
+        colorBlending.BlendConstants[1] = 0;
+        colorBlending.BlendConstants[2] = 0;
+        colorBlending.BlendConstants[3] = 0;
+
+        PipelineLayoutCreateInfo pipelineLayoutInfo = new()
+        {
+            SType = StructureType.PipelineLayoutCreateInfo,
+            SetLayoutCount = 0,
+            PushConstantRangeCount = 0,
+        };
+
+        if (vk!.CreatePipelineLayout(device, pipelineLayoutInfo, null, out pipelineLayout) != Result.Success)
+        {
+            throw new Exception("failed to create pipeline layout!");
+        }
+
+
+        vk!.DestroyShaderModule(device, fragShaderModule, null);
+        vk!.DestroyShaderModule(device, vertShaderModule, null);
+
+        SilkMarshal.Free((nint)vertShaderStageInfo.PName);
+        SilkMarshal.Free((nint)fragShaderStageInfo.PName);
+    }
+
+    private ShaderModule CreateShaderModule(byte[] code)
+    {
+        ShaderModuleCreateInfo createInfo = new()
+        {
+            SType = StructureType.ShaderModuleCreateInfo,
+            CodeSize = (nuint)code.Length,
+        };
+
+        ShaderModule shaderModule;
+
+        fixed (byte* codePtr = code)
+        {
+            createInfo.PCode = (uint*)codePtr;
+
+            if (vk!.CreateShaderModule(device, createInfo, null, out shaderModule) != Result.Success)
+            {
+                throw new Exception();
+            }
+        }
+
+        return shaderModule;
+
+    }
+
+    private SurfaceFormatKHR ChooseSwapSurfaceFormat(IReadOnlyList<SurfaceFormatKHR> availableFormats)
+    {
+        foreach (var availableFormat in availableFormats)
+        {
+            if (availableFormat.Format == Format.B8G8R8A8Srgb && availableFormat.ColorSpace == ColorSpaceKHR.SpaceSrgbNonlinearKhr)
+            {
+                return availableFormat;
+            }
+        }
+
+        return availableFormats[0];
+    }
+
+    private PresentModeKHR ChoosePresentMode(IReadOnlyList<PresentModeKHR> availablePresentModes)
+    {
+        foreach (var availablePresentMode in availablePresentModes)
+        {
+            if (availablePresentMode == PresentModeKHR.MailboxKhr)
+            {
+                return availablePresentMode;
+            }
+        }
+
+        return PresentModeKHR.FifoKhr;
+    }
+
+    private Extent2D ChooseSwapExtent(SurfaceCapabilitiesKHR capabilities)
+    {
+        if (capabilities.CurrentExtent.Width != uint.MaxValue)
+        {
+            return capabilities.CurrentExtent;
+        }
+        else
+        {
+            var framebufferSize = window!.FramebufferSize;
+
+            Extent2D actualExtent = new()
+            {
+                Width = (uint)framebufferSize.X,
+                Height = (uint)framebufferSize.Y
+            };
+
+            actualExtent.Width = Math.Clamp(actualExtent.Width, capabilities.MinImageExtent.Width, capabilities.MaxImageExtent.Width);
+            actualExtent.Height = Math.Clamp(actualExtent.Height, capabilities.MinImageExtent.Height, capabilities.MaxImageExtent.Height);
+
+            return actualExtent;
+        }
+    }
+
+    private SwapChainSupportDetails QuerySwapChainSupport(PhysicalDevice physicalDevice)
+    {
+        var details = new SwapChainSupportDetails();
+
+        khrSurface!.GetPhysicalDeviceSurfaceCapabilities(physicalDevice, surface, out details.Capabilities);
+
+        uint formatCount = 0;
+        khrSurface.GetPhysicalDeviceSurfaceFormats(physicalDevice, surface, ref formatCount, null);
+
+        if (formatCount != 0)
+        {
+            details.Formats = new SurfaceFormatKHR[formatCount];
+            fixed (SurfaceFormatKHR* formatsPtr = details.Formats)
+            {
+                khrSurface.GetPhysicalDeviceSurfaceFormats(physicalDevice, surface, ref formatCount, formatsPtr);
+            }
+        }
+        else
+        {
+            details.Formats = Array.Empty<SurfaceFormatKHR>();
+        }
+
+        uint presentModeCount = 0;
+        khrSurface.GetPhysicalDeviceSurfacePresentModes(physicalDevice, surface, ref presentModeCount, null);
+
+        if (presentModeCount != 0)
+        {
+            details.PresentModes = new PresentModeKHR[presentModeCount];
+            fixed (PresentModeKHR* formatsPtr = details.PresentModes)
+            {
+                khrSurface.GetPhysicalDeviceSurfacePresentModes(physicalDevice, surface, ref presentModeCount, formatsPtr);
+            }
+
+        }
+        else
+        {
+            details.PresentModes = Array.Empty<PresentModeKHR>();
+        }
+
+        return details;
     }
 
     private bool IsDeviceSuitable(PhysicalDevice device)
     {
         var indices = FindQueueFamilies(device);
 
-        return indices.IsComplete();
+        bool extensionsSupported = CheckDeviceExtensionsSupport(device);
+
+        bool swapChainAdequate = false;
+        if (extensionsSupported)
+        {
+            var swapChainSupport = QuerySwapChainSupport(device);
+            swapChainAdequate = swapChainSupport.Formats.Any() && swapChainSupport.PresentModes.Any();
+        }
+
+        return indices.IsComplete() && extensionsSupported && swapChainAdequate;
+    }
+
+    private bool CheckDeviceExtensionsSupport(PhysicalDevice device)
+    {
+        uint extentionsCount = 0;
+        vk!.EnumerateDeviceExtensionProperties(device, (byte*)null, ref extentionsCount, null);
+
+        var availableExtensions = new ExtensionProperties[extentionsCount];
+        fixed (ExtensionProperties* availableExtensionsPtr = availableExtensions)
+        {
+            vk!.EnumerateDeviceExtensionProperties(device, (byte*)null, ref extentionsCount, availableExtensionsPtr);
+        }
+
+        var availableExtensionNames = availableExtensions.Select(extension => Marshal.PtrToStringAnsi((IntPtr)extension.ExtensionName)).ToHashSet();
+
+        return deviceExtensions.All(availableExtensionNames.Contains);
+
     }
 
     private QueueFamilyIndices FindQueueFamilies(PhysicalDevice device)
