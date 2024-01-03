808e5c7be75b2ea762363d4f4ac2dcbcf9db93e5
kk-cpp <ke152@foxmail.com>
2024-01-03 23:02:38 +0800

    new Knitter.Common project

---
+++ src/runtime/Common/Knitter.Common/Asset/Vertex.cs
@@ -0,0 +1,10 @@
+﻿using Silk.NET.Maths;
+
+namespace Knitter.Common.Asset;
+
+public struct Vertex
+{
+    public Vector3D<float> pos;
+    public Vector3D<float> color;
+    public Vector2D<float> textCoord;
+}
