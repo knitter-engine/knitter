10853f8643e3f6cccc376dd62c18d09241f6b44b
kk-cpp <ke152@foxmail.com>
2024-01-08 22:51:52 +0800

    new Vk_RenderPass

---
+++ src/runtime/Platform/Knitter.Platform.Graphics/Interface/IRenderPass.cs
@@ -0,0 +1,8 @@
+﻿using Silk.NET.Vulkan;
+
+namespace Knitter.Platform.Graphics;
+
+public interface IRenderPass : IDisposable
+{
+    public RenderPass GetHandle();//TODO:delete
+}
---
+++ src/runtime/Platform/Knitter.Platform.Graphics/Vulkan/Vk_RenderPass.cs
@@ -0,0 +1,30 @@
+﻿using Silk.NET.Vulkan;
+
+namespace Knitter.Platform.Graphics.Vulkan;
+
+internal unsafe class Vk_RenderPass(Vk vk, Device device)
+    : IRenderPass
+{
+    private readonly Vk _vk = vk;
+    private readonly Device _device = device;
+    private RenderPass _renderPass;
+
+    public static IRenderPass? Create(Vk vk, Device device, RenderPassCreateInfo createInfo)
+    {
+        Vk_RenderPass vkRenderPass = new(vk, device);
+        if(vk.CreateRenderPass(device, createInfo, null, out vkRenderPass._renderPass) != Result.Success)
+        {
+            vkRenderPass.Dispose();
+            return null;
+        }
+
+        return vkRenderPass;
+    }
+
+    public void Dispose()
+    {
+        _vk.DestroyRenderPass(_device, _renderPass, null);
+    }
+
+    public RenderPass GetHandle() => _renderPass;
+}
